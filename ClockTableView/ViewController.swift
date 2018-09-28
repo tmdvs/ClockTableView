9fe7e82fb676234e0f0c2eafc89cec38cf435582
Tim Davies <timdavies@me.com>
2018-09-28 15:04:07 +0100

    Initial Commit

---
+++ ClockTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClockTableView
+//
+//  Created by Tim Davies on 28/09/2018.
+//  Copyright © 2018 Tim Davies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
